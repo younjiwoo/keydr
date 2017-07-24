2afc147173d5a89a65785d107dd4b87f7023f13c
younjiwoo <younjiwoo@gmail.com>
2017-07-23 17:54:49 -0700

    alert pops when the note passes under the bar with the right key press

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,19 +15,55 @@
 //= require_tree .
 
 const PATTERN_D = [1, 3, 4, 8, 9, 11, 13, 17, 18, 19]
-const PATTERN_F = [2, 4, 5, 6, 10, 13, 14, 15, 18, 20]
+// const PATTERN_F = [2, 4, 5, 6, 10, 13, 14, 15, 18, 20]
+
 
 $( document ).ready( function() {
+  const BAR = $('.bar')
 
-// First Column.
+  function ceilToTopOfElem(el) {
+    return el.offset().top
+  }
+  function ceilToBtmOfElem(el) {
+    return ceilToTopOfElem(el) + el.height()
+  }
+  function isOverlapping(el) {
+    return ceilToBtmOfElem(el) > ceilToTopOfElem(BAR)
+  }
+
   PATTERN_D.forEach( function (sec) {
-    setTimeout(function() {
-      const NEW_NOTE = $("<div class='note' />");
+    setTimeout( function() {
+      const NEW_NOTE = $('<div class="note active" />')
       $('.column-d').append(NEW_NOTE)
       NEW_NOTE.animate({
-        top: $('.game-board').height()
-      }, 5000, 'linear');
+        top: $('.game-board').height() - 50
+      }, 4500, 'linear', function() {
+        $( this ).removeClass('active')
+        $( this ).animate({
+          top: $('.game-board').height()
+        }, 500, 'linear')
+      })
     }, sec * 1000)
   })
+
+
+  $( window ).keypress( function (e) {
+    let firstActiveCls = $('.column-d .active').first()
+    if (firstActiveCls.length > 0) {
+      if (e.which === 100 && isOverlapping(firstActiveCls) ) {
+        return alert('yay! you pressed D when the note and the bar overlapped!')
+      }
+    }
+  })
+
+  // PATTERN_F.forEach( function (sec) {
+  //   setTimeout(function () {
+  //     const NEW_NOTE = $('<div class="note" />')
+  //     $('.column-f').append(NEW_NOTE)
+  //     NEW_NOTE.animate({
+  //       top: $('.game-board').height()
+  //     }, 5000, 'linear')
+  //   }, sec * 1000)
+  // })
 
 })
