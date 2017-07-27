c3a8afaa916e7a388de2942b7593b678fdc78bbe
younjiwoo <younjiwoo@gmail.com>
2017-07-27 16:14:09 -0700

    added game over screen with play again button and music stops when game is over instead of pausing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,7 +24,9 @@ $( document ).ready( function() {
   const BAR = $('.bar')
   let scoreBoard = $('.score-board')
   let score = $('.score')
+  let isGameActive = false
   $('.columnsContainer').hide()
+  $('.gameOverContainer').hide()
 
   // Helper Method.
   function ceilToTopOfElem(el) {
@@ -38,23 +40,25 @@ $( document ).ready( function() {
   }
   function generateNotes(sec, column) {
     setTimeout( function() {
-      const NEW_NOTE = $('<div class="note active" />')
-      $( column ).append(NEW_NOTE)
-      NEW_NOTE.animate({
-        top: $('.game-board').height() - 50
-      }, 4500, 'linear', function() {
-        $( this ).removeClass('active')
-        $( this ).animate({
-          top: $('.game-board').height()
-        }, 500, 'linear')
-      })
+      console.log(isGameActive)
+      if (isGameActive) {
+        const NEW_NOTE = $('<div class="note active" />')
+        $( column ).append(NEW_NOTE)
+        NEW_NOTE.animate({
+          top: $('.game-board').height() - 50
+        }, 4500, 'linear', function() {
+          $( this ).removeClass('active')
+          $( this ).animate({
+            top: $('.game-board').height()
+          }, 500, 'linear')
+        })
+      }
     }, sec * 1000)
   }
   function isGameOver() {
     return parseInt( score.html() ) < 10
   }
-  function handleKeypress(column, keycode) {
-    $( window ).keypress( function (e) {
+  function handleKeypress(column, keycode) { $( window ).keypress( function (e) {
       if ( e.which === keycode ) {
         let noteAboveBar = $(`${column} .active`).first()
         if ( noteAboveBar.length && !isGameOver() ) {
@@ -66,31 +70,37 @@ $( document ).ready( function() {
             score.addClass('pulse minus')
           }
         } else if ( isGameOver() ) {
-          return score.html('GAME OVER').addClass('minus')
+          stopGame( score.html() )
         }
       }
     })
   }
+  function showChooseSong() {
+    $('.gameOverContainer').hide()
+    $('.columnsContainer').hide()
+    $('.chooseSongContainer').show()
+  }
   function startGame() {
-    PATTERN_D.forEach( function(sec) {
-      generateNotes(sec, '.column-d')
-    })
+    score.html(100)
+    isGameActive = true
+    PATTERN_D.forEach( function(sec) { generateNotes(sec, '.column-d') })
     handleKeypress('.column-d', 100)
-
-    PATTERN_F.forEach( function(sec) {
-      generateNotes(sec, '.column-f')
-    })
+    PATTERN_F.forEach( function(sec) { generateNotes(sec, '.column-f')})
     handleKeypress('.column-f', 102)
-
-    PATTERN_J.forEach( function(sec) {
-      generateNotes(sec, '.column-j')
-    })
+    PATTERN_J.forEach( function(sec) { generateNotes(sec, '.column-j')})
     handleKeypress('.column-j', 106)
-
-    PATTERN_K.forEach( function(sec) {
-      generateNotes(sec, '.column-k')
-    })
+    PATTERN_K.forEach( function(sec) { generateNotes(sec, '.column-k')})
     handleKeypress('.column-k', 107)
+  }
+  function audioStartOver() {
+    $('audio').get(0).pause()
+    $('audio').get(0).currentTime = 0
+  }
+  function stopGame(currentScore) {
+    isGameActive = false
+    $('.gameOverContainer').show(1000)
+                           .append(`<p class='endingScore'>Your score: ${currentScore}</p>`)
+    audioStartOver()
   }
 
   score[0].addEventListener('webkitAnimationEnd', function() {
@@ -100,12 +110,11 @@ $( document ).ready( function() {
   $('.chooseSongContainer').click( function() {
     $('.chooseSongContainer').hide(1000)
     $('.columnsContainer').show(2000)
-    let easyLevel = new Audio('assets/Khalid_Location.ogg')
-    easyLevel.play()
+    $('audio').trigger('play')
     startGame()
   })
 
-  // Start Calling Methods.
+  $('.playAgainBtn').click( showChooseSong )
 
 
 
